9a33e7d119535d1cb91f44b0239ec5867a94dd2f
Aditya <adityajoshiusa@gmail.com>
2019-07-07 12:00:12 -0400

    Have set up the Dispatcher servlet and Helper Request

--- project-1-ajoshi26/src/main/java/com/revature/request/DispatcherServlet.java
+++ project-1-ajoshi26/src/main/java/com/revature/request/DispatcherServlet.java
@@ -1,6 +1,41 @@
-package com.revature.request; 
+package com.revature.request;
 
-public class DispatcherServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DispatcherServlet extends HttpServlet{
 	
-
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	IOException{
+		
+		Object data = RequestHelper.process(request);
+		
+		if(data instanceof String) {
+			String URI = (String) data;
+			request.getRequestDispatcher(URI).forward(request, response);
+		}
+		/* Else, we marshall the given POJO */
+		else {
+			response.getWriter().write(new ObjectMapper().writeValueAsString(data));
+		}
+		
+//		Customer loggedCustomer = CustomerServiceAlpha.getInstance().authenticate(
+//					new Customer(request.getParameter("username"),
+//								 request.getParameter("password"))
+//);
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	IOException{
+		doGet(request, response);
+	}
 }
--- project-1-ajoshi26/src/main/java/com/revature/request/RequestHelper.java
+++ project-1-ajoshi26/src/main/java/com/revature/request/RequestHelper.java
@@ -1,7 +1,26 @@
 package com.revature.request;
+import javax.servlet.http.HttpServletRequest;
 
+import com.revature.controllers.HomeController;
+import com.revature.controllers.LoginController;
+import com.revature.controllers.RegisterController;
 
 public class RequestHelper {
-	
 
+	private RequestHelper() {}
+
+	public static Object process(HttpServletRequest request) {
+		switch(request.getRequestURI()) {
+		case "/project-1-ajoshi26/login.do":
+			return LoginController.getInstance().login(request);
+		case "/project-1-ajoshi26/logout.do":
+			return LoginController.getInstance().logout(request);
+		case "/project-1-ajoshi26/home.do":
+			return HomeController.getInstance().home(request);
+		case "/project-1-ajoshi26/register.do":
+			return RegisterController.getInstance().register(request);
+		default:
+			return "404";
+		}
+	}
 }
