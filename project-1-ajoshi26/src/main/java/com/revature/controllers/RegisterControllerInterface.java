2c029dc5eec559fd6ed7bd0615193ab4024e3e58
Aditya <adityajoshiusa@gmail.com>
2019-07-14 17:41:53 -0400

    Added viewing and updating requests into the controller interface

--- project-1-ajoshi26/src/main/java/com/revature/controllers/RegisterControllerInterface.java
+++ project-1-ajoshi26/src/main/java/com/revature/controllers/RegisterControllerInterface.java
@@ -6,5 +6,11 @@ public interface RegisterControllerInterface {
 	public Object register(HttpServletRequest request);
 	public Object updateInfo(HttpServletRequest request);
 	public Object viewEmployees(HttpServletRequest request);
+	public Object insertRequests(HttpServletRequest request);
 	public Object updateRequests(HttpServletRequest request);
+	public Object viewRequestsbyEmployee(HttpServletRequest request);
+	public Object viewPendingRequests(HttpServletRequest request);
+	public Object viewResolvedRequests(HttpServletRequest request);
+	public Object viewPendingRequestsByID(HttpServletRequest request);
+	public Object viewResolvedRequestsByID(HttpServletRequest request);
 }
