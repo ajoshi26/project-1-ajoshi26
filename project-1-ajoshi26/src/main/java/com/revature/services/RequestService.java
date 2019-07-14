dbd286634fd1154ef5579dffaab09a2bbe2e0213
Aditya <adityajoshiusa@gmail.com>
2019-07-14 17:39:53 -0400

    Implemented methods for viewing and updating requests

--- project-1-ajoshi26/src/main/java/com/revature/services/RequestService.java
+++ project-1-ajoshi26/src/main/java/com/revature/services/RequestService.java
@@ -3,19 +3,52 @@ package com.revature.services;
 import java.util.List;
 
 import com.revature.models.Request;
+import com.revature.repositories.AccountRepositoryJDBC;
 import com.revature.repositories.RequestRepositoryJDBC;
 
 public class RequestService implements RequestServiceInterface{
+	
+	private static RequestServiceInterface requestService = new RequestService();
 
-	@Override
-	public List<Request> listAllRequests() {
-		return RequestRepositoryJDBC.getInstance2().lookAtRequest();
+	private RequestService() { }
+
+	public static RequestServiceInterface getInstance() {
+		return requestService;
 	}
 
 	@Override
+	public Request listRequestbyID(Request request) {
+		return  RequestRepositoryJDBC.getInstance2().lookAtRequestByEmployee(request);
+	}
+
+	@Override
+	public List<Request> listAllPendingRequests() {
+		return RequestRepositoryJDBC.getInstance2().lookAtPendingRequests();
+	}
+
+	@Override
+	public List<Request> listAllResolvedRequests() {
+		return  RequestRepositoryJDBC.getInstance2().lookAtResolvedRequests();
+	}
+
+	@Override
+	public Request listPendingRequestsbyID(Request request) {
+		return  RequestRepositoryJDBC.getInstance2().lookAtPendingRequestByID(request);
+	}
+
+	@Override
+	public Request listResolvedRequestsbyID(Request request) {
+		return  RequestRepositoryJDBC.getInstance2().lookAtPendingRequestByID(request);
+	}
+	
+	@Override
 	public boolean insertingRequests(Request request) {
-		// TODO Auto-generated method stub
-		return false;
+		return RequestRepositoryJDBC.getInstance2().insertRequest(request);
+	}
+
+	@Override
+	public boolean updatingRequests(Request request) {
+		return RequestRepositoryJDBC.getInstance2().updateRequest(request);
 	}
 	
 }
